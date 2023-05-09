90b9811f4de6f4da68710eae6c2150283bcd787b
randomfan022 <112581442+randomfan022@users.noreply.github.com>
2023-05-09 07:38:20 +0530

    Update p11.c

--- p11.c
+++ p11.c
@@ -1,63 +1,41 @@
-#define MAX 500 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
-#include <string.h> 
-int table[MAX]; 
-char text[MAX]; 
-char pattern[MAX]; 
-void shiftTable(char patern[],int m) 
-{ 
- int i,j; 
- for(i=0;i<MAX;i++) 
- table[i]=m; 
- 
- for(j=0;j<m-1;j++) 
- table[pattern[j]]=m-1-j;
-} 
-int Horspool(char pattern[],int m,char text[],int n) 
-{ 
- int i,j,k; 
- shiftTable(pattern,m); 
- i=m-1;
- while(i<n) 
- { 
- k=0; 
- while ((k<m) && (pattern[m-1-k]==text[i-k])) 
- { 
- k++; 
- } 
- if(k==m) 
- return i-(m-1); 
- else 
- i=i+table[text[i]]; 
- } 
-} 
-void main() 
-{ 
- int m,n,result; 
- double clk; 
- clock_t start,end; 
- 
- printf("ENTER A TEXT\n"); 
- scanf("%s",text); 
- 
- printf("ENTER PATTERN TO SEARCH\n"); 
- scanf("%s",pattern); 
- 
- m=strlen(pattern); 
- n=strlen(text); 
- 
- start=clock(); 
- result=Horspool(pattern,m,text,n); 
- end=clock(); 
- 
- if(result==-1)
- printf("PATTERN NOT FOUND\n"); 
- else 
- printf("PATTERN FOUND AT %d\n",result); 
- 
- clk=(double)(end-start)/CLOCKS_PER_SEC; 
- 
- printf("TIME REQUIRED IS %f\n",clk); 
-}
+data()
+
+mtcars
+
+row <- nrow(mtcars)
+col <- ncol(mtcars)
+
+print(row)
+print(col)
+
+automatic <- 0
+manual <- 0
+
+for(i in 1:row)
+  ifelse(mtcars[i,9] == 1, manual <- manual + 1, automatic <- automatic + 1)
+
+ifelse(automatic > manual, "more automatic", "more manual")
+
+
+x <- data.frame(mtcars)
+
+hp <- x[,4]
+weight <- x[,6]
+
+
+scatter.smooth(hp, weight, span = 2/3, degree = 1, family=c("symmetric","gaussian"))
+
+mpg <- x[,1]
+
+hist(mpg, breaks = 12, col = "lightblue", border = "pink")
+
+newmtcars = mtcars
+
+newmtcars$am = as.integer(mtcars$am)
+newmtcars$cyl = as.integer(mtcars$cyl)
+newmtcars$vs = as.integer(mtcars$vs)
+
+sapply(newmtcars,class)
+
+mtcars[mtcars$cyl<5,]
+
